692c5f05454d555dbb96e140f705eaeac6db79c9
Michael Asiamah <michaelasiamah@example.com>
2025-11-21 21:49:28 -0500

    single file of plotting commands

--- words.py
+++ words.py
@@ -1,36 +1,47 @@
-def num_letters(num_rows,num_columns):
-    return num_rows*(num_columns-1)
+def num_letters(num_rows, num_columns):
+    return num_rows * (num_columns - 1)
 
-
-assert num_letters(1,5)==4
-assert num_letters(4,4)==12
-assert num_letters(2,10)==18
-assert num_letters(5,7)==30
+assert num_letters(1, 5) == 4
+assert num_letters(4, 4) == 12
+assert num_letters(2, 10) == 18
+assert num_letters(5, 7) == 30
 
 print("Checking assertions.")
 
 import numpy as np
-data = np.loadtxt("loop4.csv", delimiter=",", dtype=str, encoding = 'utf8')
 
-words =[]
+data = np.loadtxt("loop4.csv", delimiter=",", dtype=str, encoding="utf8")
+
 rows, columns = data.shape
-cell = data[0,columns-1]
-for r in range(rows):
-    for c in range(columns):
-        if cell == (''):
-            continue
-        else:
-            words.append(str(data[r,c]))
+words = []
+r = 0
+while r < rows:
+    c = 0
+    while c < columns:
+        letter = str(data[r, c])
+        if letter != "":
+            words.append(letter)
+        c += 1
+    r += 1
 
 print(words)
-assert len(words) == rows*columns
+
+
 
 two_words = []
+r = 0
+while r < rows:
+    c = 0
+    while c < (columns - 1):
 
-for r in range(rows):
-    for c in range(columns-1):
-        word = data[r,c]+data[r,c+1]
-        two_words.append(word)
+        first = data[r, c]
+        second = data[r, c + 1]
+
+        if first != "" and second != "":
+            two_words.append(first + second)
+
+        c += 1
+    r += 1
 
 print(two_words)
-assert len(two_words)==num_letters(rows,columns)
+
